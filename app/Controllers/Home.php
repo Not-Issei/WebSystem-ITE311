74c882b420f8d337f86d3345d85621107f683542
Not-issei <talinokrish@gmail.com>
2025-08-06 14:56:59 +0800

    Added Bootstrap template and Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('home'); // Make sure 'home.php' exists in app/Views/
     }
 }
