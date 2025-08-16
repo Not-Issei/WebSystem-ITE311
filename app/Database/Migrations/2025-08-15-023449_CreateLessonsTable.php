f15268f11fa7078426cd229a44b76c5a3936b2bc
Not-Issei <talinokrish@gmail.com>
2025-08-16 13:13:42 +0800

    Initial commit - CodeIgniter setup

--- app/Database/Migrations/2025-08-15-023449_CreateLessonsTable.php
+++ app/Database/Migrations/2025-08-15-023449_CreateLessonsTable.php
@@ -8,11 +8,21 @@ class CreateLessonsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'         => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'course_id'  => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true],
+            'title'      => ['type' => 'VARCHAR', 'constraint' => '200'],
+            'content'    => ['type' => 'TEXT'],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('course_id', 'courses', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('lessons');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('lessons');
     }
 }
